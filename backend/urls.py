e4112af0adb745a7d5425e82e376b3dffad2b69d
Liviu Theodor Ene <eneliviu@gmail.com>
2024-12-21 13:06:43 +0000

    Deployment

--- backend/urls.py
+++ backend/urls.py
@@ -15,6 +15,12 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
+    path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path(
+        'dj-rest-auth/registration/',
+        include('dj_rest_auth.registration.urls')
+    ),
+
     # app urls
     path('', include('user_profile.urls')),
 
